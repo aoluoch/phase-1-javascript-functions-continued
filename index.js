dd95e69989c109633cb4bdc53457526c583c106d
Amos Oluoch <aoluoch271@gmail.com>
2024-07-01 12:08:19 +0300

    adv

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+// code your solution here 
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+console.log(saturdayFun())
+console.log(saturdayFun("Watch Euros"))
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+console.log(mondayWork()); 
+console.log(mondayWork("work from home"));
+console.log(mondayWork("attend meetings"));
+
+
+function wrapAdjective(flair = "*"){
+    return function(adjective="special"){
+        return `You are ${flair} ${adjective} ${flair}`
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!")
+console.log(encouragingPromptFunction("%")("a dedicated programmer"))
+
